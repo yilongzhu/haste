03362a67b461bb182bf5ca0dc8e01582ff23a88f
Yilong Zhu <me@yilongzhu.com>
2018-10-26 21:14:22 -0400

    Fix dependencies

--- app/__init__.py
+++ app/__init__.py
@@ -2,11 +2,14 @@ from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
 
 
 db = SQLAlchemy()
 migrate = Migrate()
 
+login = LoginManager()
+login.login_view = 'auth.login'
 
 def create_app(config_class=Config):
     application = Flask(__name__)
@@ -14,6 +17,7 @@ def create_app(config_class=Config):
 
     db.init_app(application)
     migrate.init_app(application, db)
+    login.init_app(application, db)
 
     from app.main import bp as main_bp
     application.register_blueprint(main_bp)
--- app/models.py
+++ app/models.py
@@ -1,7 +1,6 @@
-from app import login 
 from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import datetime
-from app import db
+from app import db, login
 from flask_login import UserMixin
 
 
