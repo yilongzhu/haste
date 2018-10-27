0ee8549679ae1ad99dd07a0b3789cbe8ac468636
Yilong Zhu <me@yilongzhu.com>
2018-10-27 12:31:57 -0400

    Add neworder page

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,6 +1,9 @@
 from flask import render_template, request
 from sqlalchemy import desc
+
+from app import db
 from app.main import bp
+from app.models import Item
 from flask_login import current_user, login_required
 
 
@@ -8,3 +11,17 @@ from flask_login import current_user, login_required
 @login_required
 def home():
     return render_template('home.html')
+
+
+@bp.route('/neworder', methods=['GET', 'POST'])
+@login_required
+def new_order():
+    items = Item.query.all()
+    return render_template('new_order.html', items=items)
+
+
+@bp.route('/cart', methods=['GET', 'POST'])
+@login_required
+def cart():
+    #items = Item.query.all()
+    return render_template('cart.html', items=items)
