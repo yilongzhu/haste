c2e9b4f73f352cc5f6dbef3523f75c684a638192
danielkong365 <daniel.kong@emory.edu>
2018-10-26 20:52:31 -0400

    Fix database models

--- app/models.py
+++ app/models.py
@@ -7,8 +7,11 @@ from flask_login import UserMixin
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), index=True, unique=True)
-    password_hash = db.Column(db.String(128)) 
+    phone = db.Column(db.Integer, index=True, unique=True)
+    email = db.Column(db.String(128), index=True, unique=True)
+    password_hash = db.Column(db.String(128))
+    school = db.Column(db.String(128), index=True)
+     
 
     def __repr__(self):
         return '<User {}>'.format(self.username) 
