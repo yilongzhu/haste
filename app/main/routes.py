7e332a42444a483fbbfb352baaa431db61f489c8
Yilong Zhu <me@yilongzhu.com>
2018-10-27 15:15:36 -0400

    Add neworder page

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,9 +1,9 @@
-from flask import render_template, request
+from flask import render_template, request, redirect, url_for
 from sqlalchemy.sql import collate
 
 from app import db
 from app.main import bp
-from app.models import Item
+from app.models import Item, Request, Content
 from flask_login import current_user, login_required
 
 
@@ -17,6 +17,30 @@ def home():
 @bp.route('/neworder', methods=['GET', 'POST'])
 @login_required
 def new_order():
+    newdict = {}
+    if request.method == 'POST':
+        data = request.form.to_dict()
+        #print(data)
+        for key in data:
+            if data[key] != '':
+                newdict[key] = data[key]
+                print("Added to dict")
+
+        req = Request(author=current_user)
+        db.session.add(req)
+        db.session.commit()
+
+        for key in newdict:
+            #print(key, newdict[key])
+            if (newdict[key] == 'on'):
+                temp = key+"-quantity"
+                quantity = 1 if temp not in newdict else int(newdict[temp])
+                content = Content(cart=req, item_id=int(key), quantity=int(quantity))
+                db.session.add(content)
+                db.session.commit()
+
+        return redirect(url_for('main.home'))
+
     items = Item.query.order_by(collate(Item.name, 'NOCASE')).all()
     return render_template('new_order.html', items=items)
 
--- ignition.py
+++ ignition.py
@@ -1,8 +1,8 @@
 from app import create_app, db
-from app.models import User, Item
+from app.models import User, Item, Request, Content
 
 application = create_app()
 
 @application.shell_context_processor
 def make_shell_context():
-    return {'db': db, 'User': User, 'Item': Item}
+    return {'db': db, 'User': User, 'Item': Item, 'Request': Request, 'Content': Content}
